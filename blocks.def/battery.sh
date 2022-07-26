5045fdbf690e6ab84515830ba3b9960695afeb99
Bryan Rinders <46681553+BryanRi@users.noreply.github.com>
2022-07-26 16:45:09 +0200

    add colors for high and low battery levels

--- blocks.def/battery.sh
+++ blocks.def/battery.sh
@@ -1,4 +1,15 @@
 #!/bin/sh
 ICON=""
-read -r capacity </sys/class/power_supply/BAT0/capacity
-printf "$ICON%s%%" "$capacity"
+BAT_DIR="/sys/class/power_supply/BAT0"
+read -r capacity < "$BAT_DIR/capacity"
+if [ "$(cat $BAT_DIR/status)" = "Charging" ]; then
+    [ "$capacity" -ge 85 ] && printf "\x0c AC:%s%% \x0b" "$capacity" && exit 0
+    printf "AC:%s%%" "$capacity"
+else
+    [ "$capacity" -gt 15 ] && printf "B:%s%%" "$capacity" && exit 0
+    read -r rate < "$BAT_DIR"/current_now
+    read -r val < "$BAT_DIR"/charge_now
+    hr="$(( val / rate ))"
+    mn="$(( (val % rate * 60) / rate ))"
+    printf "\x0c B:%s%% (%s:%s) \x0b" "$capacity" "$hr" "$mn" && exit 0
+fi
